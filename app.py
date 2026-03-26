23e844e1f3f76fb04e7e0b47e2364717dcaabb3a
afeefmusthafa089-star <afeefmusthafa089@gmail.com>
2026-03-26 16:49:52 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect
 import sqlite3
 from datetime import datetime
 
@@ -8,6 +8,7 @@ app = Flask(__name__)
 def init_db():
     conn = sqlite3.connect('tickets.db')
     cursor = conn.cursor()
+    
     cursor.execute('''
         CREATE TABLE IF NOT EXISTS tickets (
             id INTEGER PRIMARY KEY AUTOINCREMENT,
@@ -19,42 +20,50 @@ def init_db():
             created_at TEXT
         )
     ''')
+    
     conn.commit()
     conn.close()
 
 init_db()
 
+# Home page (form)
 @app.route('/')
-def home():
-    return render_template('form.html')
+def index():
+    return render_template('index.html')
 
-@app.route('/submit', methods=['POST'])
-def submit():
+# Add ticket
+@app.route('/add', methods=['POST'])
+def add_ticket():
     name = request.form['name']
     email = request.form['email']
     issue = request.form['issue']
     priority = request.form['priority']
-
+    
     conn = sqlite3.connect('tickets.db')
     cursor = conn.cursor()
+    
     cursor.execute('''
         INSERT INTO tickets (name, email, issue, priority, status, created_at)
-        VALUES (?, ?, ?, ?, 'Open', ?)
-    ''', (name, email, issue, priority, datetime.now()))
+        VALUES (?, ?, ?, ?, ?, ?)
+    ''', (name, email, issue, priority, "Open", datetime.now()))
+    
     conn.commit()
     conn.close()
+    
+    return redirect('/view')
 
-    return "Ticket Submitted Successfully!"
-
-@app.route('/tickets')
-def view_tickets():
+# View tickets
+@app.route('/view')
+def view():
     conn = sqlite3.connect('tickets.db')
     cursor = conn.cursor()
+    
     cursor.execute("SELECT * FROM tickets")
     tickets = cursor.fetchall()
+    
     conn.close()
-
+    
     return render_template('view.html', tickets=tickets)
 
-if __name__ == '_main_':
+if _name_ == '_main_':
     app.run(host='0.0.0.0', port=5000)
