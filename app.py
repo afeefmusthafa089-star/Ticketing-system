6dcf1290e393b1ee16aea1f3e83d857a0a87e70f
afeefmusthafa089-star <afeefmusthafa089@gmail.com>
2026-03-26 13:54:31 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,69 @@
+from flask import Flask, render_template, request, redirect
+import sqlite3
+from datetime import datetime
+
+app = Flask(__name__)
+
+# Create database
+def init_db():
+    conn = sqlite3.connect('tickets.db')
+    cursor = conn.cursor()
+    
+    cursor.execute('''
+        CREATE TABLE IF NOT EXISTS tickets (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            name TEXT,
+            email TEXT,
+            issue TEXT,
+            priority TEXT,
+            status TEXT,
+            created_at TEXT
+        )
+    ''')
+    
+    conn.commit()
+    conn.close()
+
+init_db()
+
+# Home page (form)
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+# Add ticket
+@app.route('/add', methods=['POST'])
+def add_ticket():
+    name = request.form['name']
+    email = request.form['email']
+    issue = request.form['issue']
+    priority = request.form['priority']
+    
+    conn = sqlite3.connect('tickets.db')
+    cursor = conn.cursor()
+    
+    cursor.execute('''
+        INSERT INTO tickets (name, email, issue, priority, status, created_at)
+        VALUES (?, ?, ?, ?, ?, ?)
+    ''', (name, email, issue, priority, "Open", datetime.now()))
+    
+    conn.commit()
+    conn.close()
+    
+    return redirect('/view')
+
+# View tickets
+@app.route('/view')
+def view():
+    conn = sqlite3.connect('tickets.db')
+    cursor = conn.cursor()
+    
+    cursor.execute("SELECT * FROM tickets")
+    tickets = cursor.fetchall()
+    
+    conn.close()
+    
+    return render_template('view.html', tickets=tickets)
+
+if __name__ == '__main__':
+    app.run(debug=True)
