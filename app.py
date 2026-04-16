204b8f0137111420222fb75ac8b42df2ac1eef85
afeefmusthafa089-star <afeefmusthafa089@gmail.com>
2026-04-16 16:11:04 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request, redirect, session, url_for
 import sqlite3
 from datetime import datetime
 import os
 
 app = Flask(__name__)
+app.secret_key = "mysecretkey"
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 db_path = os.path.join(BASE_DIR, "tickets.db")
@@ -25,12 +26,41 @@ def init_db():
 
 init_db()
 
+# Login details
+USERNAME = "admin"
+PASSWORD = "1234"
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    error = ""
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+
+        if username == USERNAME and password == PASSWORD:
+            session['user'] = username
+            return redirect(url_for('home'))
+        else:
+            error = "Invalid username or password"
+
+    return render_template('login.html', error=error)
+
+@app.route('/logout')
+def logout():
+    session.pop('user', None)
+    return redirect(url_for('login'))
+
 @app.route('/', methods=['GET'])
 def home():
+    if 'user' not in session:
+        return redirect(url_for('login'))
     return render_template('index.html')
 
 @app.route('/submit', methods=['POST'])
 def submit():
+    if 'user' not in session:
+        return redirect(url_for('login'))
+
     name = request.form['name']
     issue = request.form['issue']
 
@@ -39,7 +69,7 @@ def submit():
     cursor.execute("""
         INSERT INTO tickets (name, issue, status, created_at)
         VALUES (?, ?, ?, ?)
-    """, (name, issue, 'Open', datetime.now().strftime("%Y-%m-%d %H:%M:%S")))
+    """, (name, issue, 'Open', datetime.now().strftime("%d-%m-%Y %I:%M %p")))
     conn.commit()
     conn.close()
 
@@ -47,6 +77,9 @@ def submit():
 
 @app.route('/view', methods=['GET'])
 def view():
+    if 'user' not in session:
+        return redirect(url_for('login'))
+
     conn = sqlite3.connect(db_path)
     cursor = conn.cursor()
     cursor.execute("SELECT * FROM tickets")
